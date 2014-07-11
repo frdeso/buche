640e7792817e04a1b86e5b4ba03a1d58b52ead32
Francis Deslauriers <francis.deslauriers@efficios.com>
2014-07-11 00:12:10 -0400

    Add function call in testbed

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <unistd.h>
 #include "buche.h"
+void foobar()
+{
+    	int j;
+	for(j = 0; j < 3;++j)
+	{
+	    	BUCHE("iteration #%d",j)
+	}
+}
 int main()
 {
     	int i;
-	for(i = 0; i < 10; ++i)
+	for(i = 0; i < 3; ++i)
 	{
 		BUCHE("hello, %s", "World!");
-		usleep(500);
+		sleep(1);
+		foobar();
+		sleep(1);
 	}
 	return 0;
 }
